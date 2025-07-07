6aaf8bc4cf1d1ca629962ba338f9505ce7b77f95
Vito Barra <vb.vitobarra@gmail.com>
2025-07-07 16:33:03 +0200

    [FEATURE] added bot presets in main.py

--- main.py
+++ main.py
@@ -7,26 +7,24 @@ from ExampleBot.RandomBot import RandomBot
 
 
 if __name__ == "__main__":
-    depth = 3
-    bot1_name="MAX_Prestige_2_Moves"
-    bot2_name=f"AIFMoves_{depth}"
-    bot1 = MaxPrestigeBot(bot_name=bot1_name)
-    bot2 = AIFBot(bot_name=bot2_name,depth=depth)
+    depth = 2
+    maxPrestige_name = f"MAX_Prestige_2_Moves"
+    aif_name         = f"AIFBOT_Moves_{depth}"
+    random_name      = f"RandomBot"
 
-    # bot2_name = f"MAX_Prestige_2_Moves_1"
-    # bot1_name = f"MAX_Prestige_2_Moves_2"
-    # bot1 = MaxPrestigeBot(bot_name=bot1_name)
-    # bot2 = MaxPrestigeBot(bot_name=bot2_name)
+    bot_maxPrestige  = MaxPrestigeBot (bot_name=maxPrestige_name)
+    bot_aif          = AIFBot         (bot_name=aif_name,depth=depth)
+    bot_random       = RandomBot      (bot_name=random_name)
 
 
     game = Game()
-    game.register_bot(bot1)
-    game.register_bot(bot2)
+    game.register_bot(bot_aif)
+    game.register_bot(bot_maxPrestige)
 
     game.run(
-        bot1_name,
-        bot2_name,
+        maxPrestige_name,
+        aif_name,
         start_game_runner=True,
-        runs=1,
+        runs=14,
         threads=2,
     )
