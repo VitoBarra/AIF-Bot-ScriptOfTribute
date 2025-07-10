1dbaa478d44c377f12e539f3d3d25d6559e97a51
Vito Barra <vb.vitobarra@gmail.com>
2025-07-10 15:56:16 +0200

    [FEATURE] Added prior move check

--- BotCommon/CommonCheck.py
+++ BotCommon/CommonCheck.py
@@ -1,5 +1,6 @@
-﻿from scripts_of_tribute.enums import MoveEnum
-from scripts_of_tribute.move import BasicMove
+﻿from scripts_of_tribute.board import GameState
+from scripts_of_tribute.enums import MoveEnum
+from scripts_of_tribute.move import BasicMove, SimpleCardMove, MakeChoiceMoveUniqueCard, MakeChoiceMoveUniqueEffect
 
 
 def CheckForGoalState(game_state, player_id) -> bool:
@@ -8,6 +9,7 @@ def CheckForGoalState(game_state, player_id) -> bool:
 
 def NewPossibleMoveAvailable(moves:list[BasicMove]) -> bool:
     return not (len(moves) == 1 and moves[0].command == MoveEnum.END_TURN)
+
 
 
 def MatchCommand(move: BasicMove, move_list: list[BasicMove]):
@@ -26,3 +28,13 @@ def MatchCommand(move: BasicMove, move_list: list[BasicMove]):
             return next(m for m in possible_moves if m.cardsUniqueIds == move.cardsUniqueIds)
         case MoveEnum.END_TURN | _:
             return next(m for m in move_list if m.command == MoveEnum.END_TURN)
+
+def IsPriorMoves(move: BasicMove, game_state: GameState) -> bool:
+    if isinstance(move, SimpleCardMove) and move.command != MoveEnum.BUY_CARD:
+        return True
+    elif isinstance(move, MakeChoiceMoveUniqueEffect) and move.command == MoveEnum.MAKE_CHOICE:
+
+        return True
+    else:
+        #print(f"Command: {move.command}, Type: {type(move).__name__}")
+        return False
