c19e0c0cac917051b6fb627f7d4b506bd6361bd0
Vito Barra <vb.vitobarra@gmail.com>
2025-07-11 18:05:06 +0200

    [FEATURE] fixed MCTS implementation, added AIFBot with MCTS move selection

---
+++ bots/AIFBot_MCTS.py
@@ -0,0 +1,77 @@
+import os
+import random
+from math import floor
+
+from scripts_of_tribute.base_ai import BaseAI
+from scripts_of_tribute.board import GameState, EndGameState
+from scripts_of_tribute.enums import MoveEnum, PlayerEnum
+from scripts_of_tribute.move import BasicMove
+
+from BotCommon.CommonCheck import IsPriorMoves
+from MCTS import MonteCarloTreeSearch
+from BotCommon.Heuristics import utilityFunction_MIXMAXAVERAGERES
+
+class AIFBotMCTS(BaseAI):
+
+    ## ========================SET UP========================
+    def __init__(self, bot_name):
+        super().__init__(bot_name)
+        self.player_id: PlayerEnum = PlayerEnum.NO_PLAYER_SELECTED
+        self.start_of_game: bool = True
+        self.best_moves:list[BasicMove] = []
+
+    def select_patron(self, available_patrons):
+        pick = random.choice(available_patrons)
+        return pick
+
+
+    ## ========================Functionality========================
+    def play(self, game_state: GameState, possible_moves:list[BasicMove], remaining_time: int) -> BasicMove:
+        #Set Up
+        if self.start_of_game:
+            self.player_id = game_state.current_player.player_id
+            self.start_of_game = False
+
+        if len(possible_moves) == 1 and possible_moves[0].command == MoveEnum.END_TURN:
+            return possible_moves[0]
+
+        for move in possible_moves:
+            if IsPriorMoves(move,game_state):
+                # Return the first prior move encountered
+                return move
+
+        #Move Evaluation
+        monte_carlo_tree_search = MonteCarloTreeSearch(game_state, possible_moves, floor(remaining_time/len(possible_moves)), utilityFunction_MIXMAXAVERAGERES,500)
+        best_move = monte_carlo_tree_search.MonteCarloSearch()
+
+        # End of Search
+        if best_move is None:
+            best_move = next(move for move in possible_moves if move.command == MoveEnum.END_TURN)
+            print("best_move was None, returning end of turn")
+
+        return best_move
+
+    def game_end(self, end_game_state: EndGameState, final_state: GameState):
+        # Example how you can log your game for further analysis
+        log_dir = "logs"
+        os.makedirs(log_dir, exist_ok=True)
+
+        filename = f"game_log_MONTECARLO_{end_game_state.winner}.log"
+        filepath = os.path.join(log_dir, filename)
+
+        try:
+            with open(filepath, "w", encoding="utf-8") as f:
+                f.write(f"=== Game Ended ===\n")
+                f.write(f"Winner: {end_game_state.winner}\n")
+                f.write(f"Reason: {end_game_state.reason}\n")
+                f.write(f"Context: {end_game_state.AdditionalContext}\n\n")
+                f.write("=== Completed Actions ===\n")
+
+                for action in final_state.completed_actions:
+                    f.write(action + "\n")
+
+            print(f"[INFO] Game log saved to: {filepath}")
+
+        except Exception as e:
+            print(f"[ERROR] Failed to save game log: {e}")
+
--- main.py
+++ main.py
@@ -4,29 +4,32 @@
 from ExampleBot.MaxPrestigeBot import MaxPrestigeBot
 from ExampleBot.RandomBot import RandomBot
 from bots.AIFBot import AIFBot
+from bots.AIFBot_MCTS import AIFBotMCTS
 from bots.AIFBot_WithMoveSaving import AIFBotMoveSaving
 
 if __name__ == "__main__":
     depth = 2
     maxPrestige_name     = f"MAX_Prestige_2_Moves"
     aif_name             = f"AIFBOT_Moves_{depth}"
+    aif_MCTS_name        = f"AIFBOT_MCTS"
     aif_MoveSaving_name  = f"AIFBOT_moveSaving_Moves_{depth}"
     random_name          = f"RandomBot"
 
     bot_maxPrestige      = MaxPrestigeBot  (bot_name=maxPrestige_name)
     bot_aif              = AIFBot          (bot_name=aif_name,depth=depth)
+    bot_aif_MCTS         = AIFBotMCTS      (bot_name=aif_MCTS_name)
     bot_aif_MoveSaving   = AIFBotMoveSaving(bot_name=aif_MoveSaving_name,depth=depth)
     bot_random           = RandomBot       (bot_name=random_name)
 
 
     game = Game()
-    game.register_bot(bot_aif)
+    game.register_bot(bot_aif_MCTS)
     game.register_bot(bot_maxPrestige)
 
     game.run(
         maxPrestige_name,
         aif_name,
         start_game_runner=True,
-        runs=14,
-        threads=2,
+        runs=100,
+        threads=5,
     )
