c14905cd007017d2181393499b3cbbd0002d0531
Vito Barra <vb.vitobarra@gmail.com>
2025-07-19 18:31:09 +0200

    [FEATURE] added debug print in MCTS bot

--- bots/AIFBot_MCTS.py
+++ bots/AIFBot_MCTS.py
@@ -1,4 +1,5 @@
 import random
+import time
 from math import floor
 
 from scripts_of_tribute.base_ai import BaseAI
@@ -28,16 +29,19 @@ class AIFBotMCTS(BaseAI):
     ## ========================Functionality========================
     def play(self, game_state: GameState, possible_moves:list[BasicMove], remaining_time: int) -> BasicMove:
         #Set Up
+        print(f"star time: {remaining_time} ms possible moves: {len(possible_moves)}")
         if self.start_of_game:
             self.player_id = game_state.current_player.player_id
             self.start_of_game = False
 
         if len(possible_moves) == 1 and possible_moves[0].command == MoveEnum.END_TURN:
+            print("===============[End of turn]================")
             return possible_moves[0]
 
         for move in possible_moves:
             if IsPriorMoves(move):
                 # Return the first prior move encountered
+                print(f"    Prior move   found:  {move.command}")
                 return move
 
         best_choice = MakePriorChoice(game_state, possible_moves, utilityFunction_MMHVR)
@@ -47,8 +51,15 @@ class AIFBotMCTS(BaseAI):
 
 
         #Move Evaluation
+        start_time = time.perf_counter()
         monte_carlo_tree_search = MonteCarloTreeSearch(game_state, possible_moves, floor(remaining_time/len(possible_moves)), utilityFunction_MMHVR,500)
         best_move = monte_carlo_tree_search.MonteCarloSearch()
+        elapsed_time_ms = (time.perf_counter() - start_time) * 1000
+        print(f"MONTE CARLO SELECTION Elapsed:")
+        print(f"    time: {elapsed_time_ms} ms, remining time: {remaining_time} ms")
+        print(f"    state: coin {game_state.current_player.coins}, prestige: {game_state.current_player.prestige}, power: {game_state.current_player.power}")
+        print(f"    best move selected MC: {best_move.command}")
+
 
         # End of Search
         if best_move is None:
