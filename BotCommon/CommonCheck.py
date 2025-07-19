194118c70fe2982c436ccb0284ecb89896bc625d
Braviacull <a.iacullo@studenti.unipi.it>
2025-07-19 17:53:49 +0200

    [FEATURE] Added depth and branching factor estimation
    Co-authored-by: Vito Barra <vb.vitobarra@gmail.com>

--- BotCommon/CommonCheck.py
+++ BotCommon/CommonCheck.py
@@ -1,4 +1,7 @@
-﻿from scripts_of_tribute.board import GameState
+﻿import random
+
+import numpy as np
+from scripts_of_tribute.board import GameState
 from scripts_of_tribute.enums import MoveEnum
 from scripts_of_tribute.move import BasicMove, SimpleCardMove, MakeChoiceMoveUniqueCard, MakeChoiceMoveUniqueEffect
 
@@ -38,3 +41,36 @@ def IsPriorMoves(move: BasicMove, game_state: GameState) -> bool:
     else:
         #print(f"Command: {move.command}, Type: {type(move).__name__}")
         return False
+
+
+def depth_sample(game_state: GameState, possible_moves:list[BasicMove]) -> tuple[int, list[float]]:
+    current_game_state = game_state
+    current_possible_moves = possible_moves
+    sample_branching_factors= []
+
+    while len(current_possible_moves)>1:
+        sample_branching_factors.append(len(current_possible_moves))
+        current_possible_moves = [move for move in current_possible_moves if move.command != MoveEnum.END_TURN]
+        current_possible_move = random.choice(current_possible_moves)
+        current_game_state, current_possible_moves = current_game_state.apply_move(current_possible_move)
+
+
+    return len(sample_branching_factors), sample_branching_factors
+
+def estimate_depth(game_state: GameState, possible_moves:list[BasicMove]) -> tuple[int, list[float]]:
+    max_sample_depth = 0
+    bs = []
+    for i in range(5):
+        sample_depth, sample_branching_factors = depth_sample(game_state, possible_moves)
+        bs.append(sample_branching_factors)
+        if sample_depth > max_sample_depth:
+            max_sample_depth = sample_depth
+
+    # mediate on branching factors
+    bs = [np.array(x) for x in bs]
+    max_len = max(len(x) for x in bs)
+    bs_padded = np.array([np.pad(x, (0, max_len - len(x))) for x in bs])
+    bs_masked = np.where(bs_padded == 0, np.nan, bs_padded)
+    bf_mean = np.nanmean(bs_masked, axis=0).astype(float).tolist()
+
+    return max_sample_depth, bf_mean
