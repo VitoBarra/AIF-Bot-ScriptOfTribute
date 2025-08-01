20710c55712d939101ef82bc6517fd4876322a86
Braviacull <a.iacullo@studenti.unipi.it>
2025-08-01 15:44:14 +0200

    [FEATURE] Added a function that test if apply_move function has a non deterministic behaviour. Random Bot now discards end turn move when there are alternatives

--- ExampleBot/RandomBot.py
+++ ExampleBot/RandomBot.py
@@ -1,13 +1,24 @@
 import random
 
 from scripts_of_tribute.base_ai import BaseAI
+from scripts_of_tribute.enums import MoveEnum
+
 
 class RandomBot(BaseAI):
-    
+
     def select_patron(self, available_patrons):
         return random.choice(available_patrons)
-    
+
+    # def play(self, game_state, possible_moves, remaining_time):
+    #     pick = random.choice(possible_moves)
+    #     return pick
+
     def play(self, game_state, possible_moves, remaining_time):
+        if len(possible_moves) == 1:
+            return possible_moves[0]
+        for move in possible_moves:
+            if move.command == MoveEnum.END_TURN:
+                possible_moves.remove(move)
         pick = random.choice(possible_moves)
         return pick
     
