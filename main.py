d4f8312ea6a50a02a1b81a7c887bc5bcd335fb9b
Braviacull <a.iacullo@studenti.unipi.it>
2025-08-05 17:23:51 +0200

    [FEATURE] added new instantiated bots

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from HeuristicLerning.EvolutionaryHeuristic import evolutionary_algorithm
 from Helper.LoggerFilesHelper import CleanUpLogs
 from bots.AIFBot_MMHVR import AIFBot_MMHVR
 from bots.AIFBot_MCTS import AIFBotMCTS
+from bots.BoundedDS import BoundedDS
 
+from BotCommon.Heuristics import CalculateWeightedUtility_MMHVR
+from BotCommon.Heuristics import utilityFunction_PrestigeAndPower
 
 #=====Settings=====
 HIDE_PRINT = True
@@ -19,17 +22,25 @@ def Evolve():
 
 def MakeRun():
     depth = 2
-    maxPrestige_name     = f"MAX_Prestige_2_Moves"
-    aif_MMHVR_name       = f"AIFBOT_MMHVR_{depth}_Moves"
-    aif_MCTS_name        = f"AIFBOT_MCTS"
-    random_name          = f"RandomBot"
 
-    bot_maxPrestige      = MaxPrestigeBot  (bot_name=maxPrestige_name)
-    bot_MMHVR_aif        = AIFBot_MMHVR    (bot_name=aif_MMHVR_name, depth=depth)
-    bot_aif_MCTS         = AIFBotMCTS      (bot_name=aif_MCTS_name)
-    bot_random           = RandomBot       (bot_name=random_name)
+    maxPrestige_name           = f"MAX_Prestige_{depth}_Moves"
+    BoundedDS_WMMHVR_name       = f"BoundedDS_WMMHVR_{depth}_Moves"
+    aif_MMHVR_name             = f"AIFBOT_MMHVR_{depth}_Moves"
+    aif_MCTS_WMMHVR_name       = f"AIFBOT_MCTS_WMMHVR"
+    aif_MCTS_Max_Prestige_name = f"AIFBOT_MCTS_Max_Prestige"
+    random_name                = f"RandomBot"
 
-    RunGames(bot_aif_MCTS, bot_maxPrestige, runs=RUN_NUM, threads=THREAD_NUM, hide_print=HIDE_PRINT)
+    bot_maxPrestige            = MaxPrestigeBot               (bot_name=maxPrestige_name)
+    bot_BoundedDS_WMMHVR     = BoundedDS                      (bot_name=BoundedDS_WMMHVR_name, depth=depth,
+                                                               evaluation_function=CalculateWeightedUtility_MMHVR)
+    bot_MMHVR_aif              = AIFBot_MMHVR                 (bot_name=aif_MMHVR_name, depth=depth)
+    bot_aif_MCTS_WMMHVR        = AIFBotMCTS                   (bot_name=aif_MCTS_WMMHVR_name,
+                                                               evaluation_function= CalculateWeightedUtility_MMHVR)
+    bot_aif_MCTS_Max_Prestige  = AIFBotMCTS                   (bot_name=aif_MCTS_Max_Prestige_name,
+                                                               evaluation_function= utilityFunction_PrestigeAndPower)
+    bot_random                 = RandomBot                    (bot_name=random_name)
+
+    RunGames(bot_aif_MCTS_WMMHVR, bot_maxPrestige, runs=RUN_NUM, threads=THREAD_NUM, hide_print=HIDE_PRINT)
 
 
 if __name__ == "__main__":
