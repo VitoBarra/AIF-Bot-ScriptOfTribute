f53a5924e03a6a08c70432e1e48affffa11755d1
VitoBarra <vb.vitobarra@gmail.com>
2025-08-10 20:11:26 +0200

    [FEATURE] added logger functionally in a dedicated file

--- Helper/Logging.py
+++ Helper/Logging.py
@@ -3,17 +3,17 @@ import os
 
 from scripts_of_tribute.board import EndGameState, GameState
 
-from Helper import LOG_FOLDER_NAME
+from Helper import LOG_FOLDER_NAME, LOG_ENABLED_FILE, LOG_ENABLED_PRINT
 
 
 def LogEndOfGame(bot_name:str, end_game_state: EndGameState, final_state: GameState):
-    # Example how you can log your game for further analysis
-    log_dir = LOG_FOLDER_NAME
-    os.makedirs(log_dir, exist_ok=True)
+    if not LOG_ENABLED_FILE:
+        return
+    os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
 
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"{bot_name}_{current_time}_{end_game_state.winner}.log"
-    filepath = os.path.join(log_dir, filename)
+    filepath = os.path.join(LOG_FOLDER_NAME, filename)
 
     try:
         with open(filepath, "w", encoding="utf-8") as f:
@@ -26,7 +26,16 @@ def LogEndOfGame(bot_name:str, end_game_state: EndGameState, final_state: GameSt
             for action in final_state.completed_actions:
                 f.write(action + "\n")
 
-        print(f"[INFO] Game log saved to: {filepath}")
+        PrintLog(f"INFO", f"Game log saved to: {filepath}",0)
 
     except Exception as e:
-        print(f"[ERROR] Failed to save game log: {e}")
+        PrintLog("ERROR",f"Failed to save game log: {e}",0)
+        
+        
+        
+
+def PrintLog(logType: str,logMessage: str, tab_count: int = 0):
+    if not LOG_ENABLED_PRINT:
+        return
+    tabs = "\t" * tab_count
+    print(f"{tabs}[{logType:^{8}}] -> {logMessage}")
--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,1 +1,6 @@
+import os
+
 LOG_FOLDER_NAME = "logs"
+LOG_ENABLED_FILE = False
+LOG_ENABLED_PRINT = False
+
